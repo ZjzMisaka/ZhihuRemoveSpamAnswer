a4d50ad7df8c16888e18c47af392162757479ce0
ZjzMisaka <530514257@qq.com>
2022-02-20 13:35:08 +0800

    Update ZhihuRemoveSpamAnswer.js

--- ZhihuRemoveSpamAnswer.js
+++ ZhihuRemoveSpamAnswer.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         屏蔽知乎刷屏与大量复读的回答
+// @name         屏蔽知乎无意义回答
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  屏蔽知乎刷屏与大量复读的回答
+// @version      0.2
+// @description  屏蔽知乎无意义回答, 包括刷屏与大量复读的回答, 嘲讽类定型文等.
 // @author       ZjzMisaka
 // @match        https://www.zhihu.com/*
 // @icon         https://www.google.com/s2/favicons?domain=zhihu.com
@@ -19,60 +19,53 @@
     Ana();
 
     function Ana(){
-        var richContentList = document.getElementsByClassName("RichContent-inner");
+        var richContentList = document.getElementsByClassName("RichContent");
         for (var k = 0; k < richContentList.length; ++k) {
+            var needRemove = false;
             var contentText = richContentList[k];
-            var contentInnerText = contentText.innerText.substring(contentText.innerText.indexOf('：') + 2);
-            var contentFirstText = contentInnerText[0];
-            var contentSplitList = contentInnerText.split(contentFirstText);
-
-            var contentCount = 0;
-            var contentTemp = contentSplitList[1];
-            for (var jj = 1; jj < contentSplitList.length; ++jj) {
-                if(contentSplitList[jj] == contentTemp){
-                    ++contentCount;
-                } else {
-                    break;
-                }
-                if(contentCount == 6){
-                    var contentEleTemp = contentText;
-                    while (!contentEleTemp.classList.contains('TopstoryItem') && !contentEleTemp.classList.contains('List-item')){
-                        contentEleTemp = contentEleTemp.parentNode;
-                    }
-                    contentEleTemp.parentNode.removeChild(contentEleTemp)
-
-                    break;
-                }
+            var contentInnerText = "";
+            if(contentText.classList.contains('is-collapsed')) {
+                contentInnerText = contentText.innerText.substring(contentText.innerText.indexOf('：') + 1);
+            }else {
+                contentInnerText = contentText.innerText;
             }
+            needRemove = Res1(contentInnerText) || Res2(contentInnerText);
 
+            if(needRemove) {
+                var contentEleTemp = contentText;
+                while (!contentEleTemp.classList.contains('TopstoryItem') && !contentEleTemp.classList.contains('List-item')){
+                    contentEleTemp = contentEleTemp.parentNode;
+                }
+                contentEleTemp.parentNode.removeChild(contentEleTemp)
+            }
         }
+    }
 
-        var richTextList = document.getElementsByClassName("CopyrightRichText-richText");
-        for (var i = 0; i < richTextList.length; ++i) {
-            var richText = richTextList[i];
-            var firstText = richText.innerText[0];
-            var innerText = richText.innerText;
-            var splitList = innerText.split(firstText);
+    function Res1(contentInnerText) {
+        var contentFirstText = contentInnerText[0];
+        var contentSplitList = contentInnerText.split(contentFirstText);
 
-            var count = 0;
-            var temp = splitList[1];
-            for (var j = 1; j < splitList.length; ++j) {
-                if(splitList[j] == temp){
-                    ++count;
-                } else {
-                    break;
-                }
-                if(count == 6){
-                    var eleTemp = richText;
-                    while (!eleTemp.classList.contains('TopstoryItem') && !eleTemp.classList.contains('List-item')){
-                        eleTemp = eleTemp.parentNode;
-                    }
-                    eleTemp.parentNode.removeChild(eleTemp)
-
-                    break;
-                }
+        var contentCount = 0;
+        var contentTemp = contentSplitList[1];
+        for (var jj = 1; jj < contentSplitList.length; ++jj) {
+            if(contentSplitList[jj] == contentTemp){
+                ++contentCount;
+            } else {
+                break;
             }
+            if(contentCount == 6){
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
 
+    function Res2(contentInnerText) {
+        if(contentInnerText.startsWith("世界卫生组织（WHO）根据智商将精神发育迟滞分为以下四个等级")) {
+            return true;
+        } else {
+            return false;
         }
     }
 })();
